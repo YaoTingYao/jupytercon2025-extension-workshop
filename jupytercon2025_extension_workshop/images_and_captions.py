023c9f7b5c0f8dd15558b668a4df9409a206e9db
YaoTingYao <leannayao@gmail.com>
2025-11-03 14:05:41 -0800

    Add images

---
+++ jupytercon2025_extension_workshop/images_and_captions.py
@@ -0,0 +1,6 @@
+IMAGES_AND_CAPTIONS = [
+    { "filename": "brunnhilde.jpg", "caption": "Brünnhilde" },
+    { "filename": "cats.jpg", "caption": "Cats" },
+    { "filename": "cat-cher-evolution.jpg", "caption": "Evolution of a cat-cher" },
+    { "filename": "the-entanglement.jpg", "caption": "The entanglement" },
+]
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import {
+  ILayoutRestorer,
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-import { ICommandPalette } from '@jupyterlab/apputils';
+import { ICommandPalette,
+       WidgetTracker} from '@jupyterlab/apputils';
 import { ILauncher } from '@jupyterlab/launcher'
 import { requestAPI } from './request';
 import { ImageCaptionMainAreaWidget } from './widget';
@@ -16,11 +18,13 @@ const plugin: JupyterFrontEndPlugin<void> = {
   description: 'A JupyterLab extension that displays a random image and caption.',
   autoStart: true,
     requires: [ICommandPalette, ILauncher],  // dependencies of our extension
+    optional: [ILayoutRestorer],
   activate: (app: JupyterFrontEnd,
             // The activation method receives dependencies in the order they are specified in
       // the "requires" parameter above:
       palette: ICommandPalette, 
-      launcher: ILauncher) => {
+      launcher: ILauncher,
+      restorer: ILayoutRestorer | null) => {
     console.log('JupyterLab extension jupytercon2025-extension-workshop is activated!');
 
     requestAPI<any>('hello')
@@ -32,12 +36,30 @@ const plugin: JupyterFrontEndPlugin<void> = {
           `The jupytercon2025_extension_workshop server extension appears to be missing.\n${reason}`
         );
       });
+
+      // Track widget state
+    const tracker = new WidgetTracker<ImageCaptionMainAreaWidget>({
+      namespace: 'jupytercon2025-extension-workshop'
+    });
+          
       //Register a new command:
     const command_id = 'image-caption:open';
     app.commands.addCommand(command_id, {
-      execute: () => {
+      execute: (args?: { id?: string }) => {
         // When the command is executed, create a new instance of our widget
         const widget = new ImageCaptionMainAreaWidget();
+
+        // Use provided ID or generate a new one
+        // During restoration, the args will contain the saved widget ID
+        if (args && args.id) {
+          widget.id = args.id;
+        } else {
+          widget.id = `image-caption-${crypto.randomUUID()}`;
+        }
+
+        if (!tracker.has(widget)) {
+          tracker.add(widget);
+        }
 
         // Then add it to the main area:
         app.shell.add(widget, 'main');
@@ -48,6 +70,15 @@ const plugin: JupyterFrontEndPlugin<void> = {
 
     palette.addItem({ command: command_id, category: 'Tutorial' });
     launcher.add({ command: command_id });
+
+    // Restore widget state
+    if (restorer) {
+      restorer.restore(tracker, {
+        command: command_id,
+        args: widget => ({ id: widget.id }),
+        name: widget => widget.id
+      });
+    }
   }
 };
 
